e4378d90725ec1ad7e322ea182e4a94746ceee92
Austin Jackowski <ajackowski@gmail.com>
2019-02-22 18:24:28 -0500

    Add createGrid() function to make a grid of visible divs

--- core.js
+++ core.js
@@ -1,13 +1,25 @@
 // VARIABLES
 
-const clearBtn = document.querySelector('#clear')
+const createClearBtn = document.querySelector('#create-clear')
 const colorBtn = document.querySelector('#color')
 const gridContainer = document.querySelector('.grid-container')
-let gridSize
-let gridPixel
 
 // EVENT LISTENERS
 
 
 
 // FUNCTIONS
+
+function createGrid (gridSize) {
+  let i
+  let gridPixel
+
+  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+
+  for (i = 0; i < (gridSize ** 2); i++) {
+    gridPixel = document.createElement('div')
+    gridPixel.style.height = (600 / gridSize).toString() + 'px'
+    gridPixel.classList.add('pixel')
+    gridContainer.appendChild(gridPixel)
+  }
+}
