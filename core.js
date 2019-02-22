07a58c6fa45dea3bece61c26104df307a9a23aa3
Austin Jackowski <ajackowski@gmail.com>
2019-02-22 16:11:01 -0500

    Add more initial variables to core.js

--- core.js
+++ core.js
@@ -1,8 +1,13 @@
 // VARIABLES
 
-const clearBtn = document.querySelector('#clear');
-const colorBtn = document.querySelector('#color');
+const clearBtn = document.querySelector('#clear')
+const colorBtn = document.querySelector('#color')
+const gridContainer = document.querySelector('.grid-container')
+let gridSize
+let gridPixel
 
 // EVENT LISTENERS
+
+
 
 // FUNCTIONS
