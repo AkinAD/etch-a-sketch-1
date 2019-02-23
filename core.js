da3538e6c14e980c68a2e0967c1a02b7bdee6bcf
Austin Jackowski <ajackowski@gmail.com>
2019-02-22 19:06:20 -0500

    Add control flow to make clear/create button work as intended

--- core.js
+++ core.js
@@ -10,17 +10,23 @@ createClearBtn.addEventListener('click', createGrid)
 
 // FUNCTIONS
 
-function createGrid () {
-  let i
-  let gridPixel
-  let gridSize = prompt('Enter the size of grid you want!')
+function createGrid() {
+  if (!gridContainer.hasChildNodes()) {
+    let i
+    let gridPixel
+    let gridSize = prompt('Enter the size of grid you want!')
 
-  gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
 
-  for (i = 0; i < (gridSize ** 2); i++) {
-    gridPixel = document.createElement('div')
-    gridPixel.style.height = (600 / gridSize).toString() + 'px'
-    gridPixel.classList.add('pixel')
-    gridContainer.appendChild(gridPixel)
+    for (i = 0; i < (gridSize ** 2); i++) {
+      gridPixel = document.createElement('div')
+      gridPixel.style.height = (600 / gridSize).toString() + 'px'
+      gridPixel.classList.add('pixel')
+      gridContainer.appendChild(gridPixel)
+    }
+  } else {
+    while (gridContainer.firstChild) {
+      gridContainer.removeChild(gridContainer.firstChild)
+    }
   }
 }
